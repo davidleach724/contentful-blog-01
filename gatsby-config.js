78805b9091df2a9bcd9adf7747ffa43170de5ac1
David Leach <david.leach@Davids-MacBook-Pro.local>
2022-03-07 08:27:30 -0700

    Install gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const dotenv = require('dotenv')
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
@@ -128,5 +134,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `tn4xnojdvoud`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    `@contentful/gatsby-transformer-contentful-richtext`
   ],
 }
